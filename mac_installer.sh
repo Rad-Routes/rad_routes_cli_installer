f3df9a0f1e694c03360a329520fbc1986812204a
Shyaboi <ian.shane.sears@gmail.com>
2021-03-31 01:30:13 -0700

    checking and installing pipenv

--- mac_installer.sh
+++ mac_installer.sh
@@ -1,3 +1,23 @@
+vers=`pipenv --version`
+
+echo $vers | awk '{print substr($0,length,1)}'
+
+if [ "$vers" == "d" ]; then
+
+echo 'no pippppppppppppp env '
+
+echo 'Installing pipenv'
+
+pip3 install pipenv
+
+else
+
+echo $vers
+
+fi
+
+echo 'done checking for pipenv...'
+
 mkdir -p ~/.rad_routes
 
 cd ~/.rad_routes
