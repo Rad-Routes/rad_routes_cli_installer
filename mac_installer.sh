0104fb929a6f7d9e3a8c13a20fe64541942021b5
Shyaboi <ian.shane.sears@gmail.com>
2021-03-30 21:22:33 -0700

    initial comit, branchin from win install, made simple mac installer

---
+++ mac_installer.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.rad_routes
+
+cd ~/.rad_routes
+
+curl https://github.com/shyaboi/rad-route-runner/archive/refs/heads/master.zip -O -J -L
+
+unzip rad-route-runner-master
+
+chmod +x rr.py
+
+mv rr.py rr
+
+ln -s ~/.rad_routes/rad-route-runner-master/rr /usr/local/bin
+
+exit 0
