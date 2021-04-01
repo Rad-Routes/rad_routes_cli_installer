ca6563b44f2cfa84b8b75782b14cb16e2321bf35
Shyaboi <ian.shane.sears@gmail.com>
2021-04-01 16:40:12 -0700

    changed to if and case for read var, working in mac shell

--- mac_installer.sh
+++ mac_installer.sh
@@ -6,70 +6,65 @@ echo "If you plan to use env mode, then say No."
 
 echo "Do you wish to install this program?"
 
-read benis
+read -r -p "Are you sure? [y/N] " response
 
-echo $benis
+case "$response" in
+    [yY][eE][sS]|[yY])
+    echo $response
 
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) echo "R.A.D. Routes will now install Python modules"
+    echo "R.A.D. Routes will now install Python modules"
 
-          echo "requests & pymongo"
+    echo "requests & pymongo"
 
-          pip3 install requests
+    pip3 install requests
 
-          pip3 install pymongo
+    pip3 install pymongo
 
-          echo 'done installing requests & pymongo'
+    echo 'done installing requests & pymongo'
 
-          echo '...creating R.A.D. Routes directory'
+    echo '...creating R.A.D. Routes directory'
 
-          mkdir -p ~/.rad_routes
+    mkdir -p ~/.rad_routes
 
-          cd ~/.rad_routes
+    cd ~/.rad_routes
 
-          echo 'curling the master files'
+    echo 'curling the master files'
 
-          curl https://github.com/shyaboi/rad-route-runner/archive/refs/heads/master.zip -O -J -L
+    curl https://github.com/shyaboi/rad-route-runner/archive/refs/heads/master.zip -O -J -L
 
-          echo 'unzipppping'
+    echo 'unzipppping'
 
-          unzip rad-route-runner-master
+    unzip rad-route-runner-master
 
-          cd rad-route-runner-master
+    cd rad-route-runner-master
 
-          echo 'copying files into your usr/local/bin directory'
+    echo 'copying files into your usr/local/bin directory'
 
-          chmod +x rr.py
+    chmod +x rr.py
 
-          mv rr.py rr
+    mv rr.py rr
 
-          echo 'File linking R.A.D. Routes installation files'
+    echo 'File linking R.A.D. Routes installation files'
 
-          sudo ln -s ~/.rad_routes/rad-route-runner-master/rr /usr/local/bin
+    sudo ln -s ~/.rad_routes/rad-route-runner-master/rr /usr/local/bin
 
-          echo 'R.A.D. Routes ready to use!, open a new terminal, and type rr -r YOUR_FAVORITE_ROUTE'
+    echo 'R.A.D. Routes ready to use!, open a new terminal, and type rr -r YOUR_FAVORITE_ROUTE'
 
-          break;;
-      
-      
-        No ) echo 'nooooooooooooo'
+esac
 
-          vers=`pipenv --version`
+case "$response" in
+    [nN][oO]|[nN]) 
+    echo 'nooooooooooooo'
+    vers=`pipenv --version`
 
-          echo $vers
-          # echo $vers | awk '{print substr($0,length,1)}'
+    echo $vers
+    # echo $vers | awk '{print substr($0,length,1)}'
 
-          # if [ "$vers" == "d" ]; then
+    # if [ "$vers" == "d" ]; then
 
-          echo 'Your current pipenv is ' $vers
+    echo 'Your current pipenv is ' $vers
 
-          echo 'Please use your own Python env'
-          echo 'in your virtual environment run;'
-          echo 'pip install requests'
-          echo 'pip install pymongo'
-          exit;;
+    echo 'Please use R.A.D. Routes in env mode, or install requests and pymongo in your chosen python environment'
+esac
 
-        * ) echo "invalid";;
-    esac
-done
+exit
